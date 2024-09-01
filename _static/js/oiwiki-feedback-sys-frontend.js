9a77eef661c30c0a87e1dc19944f7236cc5bf2be
Enter-tainer <Enter-tainer@users.noreply.github.com>
2024-09-01 07:27:18 +0000

    deploy: 2a3a6435846900fd2b75e934c5f0aa8326888278

---
+++ _static/js/oiwiki-feedback-sys-frontend.js
@@ -0,0 +1,51 @@
+function matchColor() {
+  const palettle = localStorage.getItem("/.__palette");
+  if (
+    (palettle !== null && JSON.parse(palettle)?.color?.scheme !== "default") ||
+    (palettle === null && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("review_dark_mode");
+  } else {
+    document.documentElement.classList.remove("review_dark_mode");
+  }
+}
+
+function hookMkdocsMaterial() {
+  document.querySelector(".md-header__option").addEventListener("click", e => {
+    if (!(e.target instanceof HTMLInputElement)) return;
+    setTimeout(matchColor, 0); // wait for the theme to be applied
+  });
+}
+
+function stringToHash(string) {
+
+  let hash = 0;
+
+  if (string.length == 0) return hash;
+
+  for (i = 0; i < string.length; i++) {
+      char = string.charCodeAt(i);
+      hash = ((hash << 5) - hash) + char;
+      hash = hash & hash;
+  }
+
+  return hash;
+}
+
+if(localStorage.getItem("giscus-session") && stringToHash(localStorage.getItem("giscus-session")) % 100 < 10){
+  localStorage.setItem("enable_paragraph_review", "true");
+}
+
+if (localStorage.getItem("enable_paragraph_review") === "true") {
+  hookMkdocsMaterial();
+
+  document$.subscribe(function () {
+    matchColor();
+
+    globalThis["OIWikiFeedbackSysFrontend"] instanceof Object &&
+    OIWikiFeedbackSysFrontend.setupReview instanceof Function &&
+    OIWikiFeedbackSysFrontend.setupReview(document.body, {
+        apiEndpoint: "https://cloudflare-workers.hikarilan.workers.dev/" // api endpoint injected here, see: scripts/post-build/inject-feedback-sys-frontend/task-handler.ts
+      });
+  });
+}
